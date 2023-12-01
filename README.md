4318c0e92ff2a59c60ce50476290f7dbbb1129d4
plusleft <107749872+0xleft@users.noreply.github.com>
2023-12-01 23:07:34 +0000

    woopwoop

---
+++ README.md
@@ -0,0 +1,9 @@
+# About
+
+A weird attempt at experimenting with assembly and recreating common tool - port scanner.
+
+## Usage
+
+```bash
+./lpfe <ip>
+```
--- src/lpfe.asm
+++ src/lpfe.asm
@@ -8,8 +8,8 @@ exitingMsg db "Exiting", 0x0
 successMsg db "Success ", 0x0
 
 section .bss
-ip resb 16
-port resb 32
+ip_addr resb 32
+port resb 8
 
 section .text
 global _start
@@ -27,8 +27,7 @@ _start:
     cmp eax, 0h
     jz noIp ; no ip provided
 
-    mov [ip], eax
-    ; debug
+    mov [ip_addr], eax ; set ip address
     call sprintln
 
     mov edx, 8080 ; starting port
@@ -82,24 +81,30 @@ _socket:
 _connect:
     mov     edi, eax            ; move return value of SYS_SOCKETCALL into edi (file descriptor for new socket, or -1 on error)
     
-    ; push ip 127.0.0.1
-    push dword 0x0100007f
+    ; decode ip address to int
+    mov edi, [ip_addr]
+    call str_to_int
+    mov edi, eax
 
-    push word [port]
-    push word 2              ; push 2 dec onto stack AF_INET
-    mov ecx, esp            ; move address of stack pointer into ecx
-    push byte 16             ; push 16 dec onto stack (arguments length)
-    push ecx                 ; push the address of arguments onto stack
-    push edi                 ; push the file descriptor onto stack
-    mov ecx, esp            ; move address of arguments into ecx
-    mov ebx, 3              ; invoke subroutine CONNECT (3)
-    mov eax, 102            ; invoke SYS_SOCKETCALL (kernel opcode 102)
-    int 80h                 ; call the kernel
+    ; move edi back
+    mov edi, eax
+
+    push    dword eax         ; push ip address onto stack
+    push    word [port]
+    push    word 2              ; push 2 dec onto stack AF_INET
+    mov     ecx, esp            ; move address of stack pointer into ecx
+    push    byte 16             ; push 16 dec onto stack (arguments length)
+    push    ecx                 ; push the address of arguments onto stack
+    push    edi                 ; push the file descriptor onto stack
+    mov     ecx, esp            ; move address of arguments into ecx
+    mov     ebx, 3              ; invoke subroutine CONNECT (3)
+    mov     eax, 102            ; invoke SYS_SOCKETCALL (kernel opcode 102)
+    int     80h                 ; call the kernel
 
     cmp eax, 0x0
     jnl .success
 
-    add esp, 20
+    add esp, 20 ; move stack pointer back
 
     ret
 
@@ -122,4 +127,24 @@ _close:
     cmp eax, 0x0
     jl socketCloseError
 
+    ret
+
+str_to_int:
+    xor eax, eax
+    xor ebx, ebx
+    xor ecx, ecx
+
+next_digit:
+    movzx edx, byte [edi + ecx]  ; Load the next byte (character) from the buffer into edx
+    cmp dl, 0           ; Check if it's the null terminator (end of string)
+    je  done             ; If it is, we are done
+
+    sub dl, '0'         ; Convert ASCII character to integer ('0' -> 0, '1' -> 1, ..., '9' -> 9)
+    imul eax, ebx        ; Multiply the current result by 10 (shift left by one decimal place)
+    add eax, edx         ; Add the new digit to the result
+
+    inc ecx             ; Move to the next character in the string
+    jmp next_digit      ; Repeat the process for the next digit
+
+done:
     ret
